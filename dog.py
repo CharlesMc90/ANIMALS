d82756354c0cda6e7bffead2c7e1977120ce0fd5
CharlesMc90 <charlesmcneil@mail.weber.edu>
2024-11-25 20:44:38 -0700

    Create dog.py

---
+++ dog.py
@@ -0,0 +1,12 @@
+from animal import Animal
+
+class Dog(Animal):
+    def __init__(self, name, species, breed):
+        super().__init__(name, species)  # Initialize name and species from Animal
+        self.breed = breed
+
+    def speak(self):
+        return "The dog barks."
+
+    def __str__(self):
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}', Breed: '{self.breed}'"
