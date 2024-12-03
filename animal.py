c3de23eee0f88a161f3f8dc5d5c231302c0937b0
CharlesMc90 <charlesmcneil@mail.weber.edu>
2024-12-02 23:34:23 -0700

    Update animal.py

--- animal.py
+++ animal.py
@@ -2,15 +2,15 @@ class Animal:
     # Class-level attribute to store all Animal instances
     all_animals = []
 
-    def __init__(self, name, species):
+    def __init__ (self, name, species):
         self.name = name
         self.species = species
         self.kingdom = "Animalia"  # All animals belong to the Animalia kingdom
         # Add the instance to the class-level list
         Animal.all_animals.append(self)
 
-    def speak(self):
+    def speak (self):
         return "The animal makes a noise."
 
-    def __str__(self):
+    def __str__ (self):
         return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}'"
